e3d9748d3f55b167930090291efab3a5d70e3818
Pavel Puropatin <kuropatin.pv@gmail.com>
2021-05-08 00:02:12 +0300

    Delete unused method

--- src/main/java/com/kuropatin/jdbc/repository/UserRepository.java
+++ src/main/java/com/kuropatin/jdbc/repository/UserRepository.java
@@ -2,11 +2,7 @@ package com.kuropatin.jdbc.repository;
 
 import com.kuropatin.jdbc.domain.User;
 
-import java.util.List;
-
 public interface UserRepository extends CrudOperations<Long, User> {
 
-    List<User> findUsersByQuery(String query);
-
-    Double getUserExpensiveCarPrice(Integer userId);
+    Double getUserExpensiveCarPrice(long userId);
 }
